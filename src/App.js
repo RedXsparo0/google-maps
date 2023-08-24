d0f4226f17855ed05fc53cedcc84df2b7b7235b7
RedXsparo0 <137707323+RedXsparo0@users.noreply.github.com>
2023-08-24 14:53:13 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import GoogleMapLoader from 'react-google-maps-loader';
+import GoogleMapReact from 'google-map-react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
 
-function App() {
+const mapStyles = {
+  width: '100%',
+  height: '400px',
+};
+
+const defaultCenter = {
+  lat: 37.7749,
+  lng: -122.4194,
+};
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container component="main" maxWidth="md">
+      <CssBaseline />
+      <Paper elevation={3} style={{ padding: '20px', margin: '20px 0' }}>
+        <Typography variant="h5" gutterBottom>
+          Welcome to My Google Maps Page!
+        </Typography>
+        <div style={mapStyles}>
+          <GoogleMapLoader
+            apiKey="YOUR_GOOGLE_MAPS_API_KEY"
+            render={googleMaps =>
+              googleMaps && (
+                <GoogleMapReact
+                  defaultCenter={defaultCenter}
+                  defaultZoom={12}
+                  bootstrapURLKeys={{ key: 'YOUR_GOOGLE_MAPS_API_KEY' }}
+                >
+                  
+                </GoogleMapReact>
+              )
+            }
+          />
+        </div>
+      </Paper>
+    </Container>
   );
-}
+};
 
 export default App;
